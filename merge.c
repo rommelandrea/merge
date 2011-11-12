b73dc8274968e2a5078c7f52f93b4f04e008055c
Andrea Romanello <rommelandrea@gmail.com>
2011-11-12 10:14:33 +0100

    inizializzazione progetto

---
+++ merge.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   merge.c
+ * Author: rommel
+ *
+ * Created on November 12, 2011, 10:13 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
